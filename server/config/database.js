3322decc20bdc549d6b3e89ae041419800010922
abu-bakr-dev <abubakarse005@gmail.com>
2025-09-02 01:23:53 +0500

    Update database path configuration for SQLite database

--- server/config/database.js
+++ server/config/database.js
@@ -1,6 +1,8 @@
 const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
-const dbPath = path.resolve(__dirname, "../database.sqlite");
+// const dbPath = path.resolve(__dirname, "../database.sqlite");
+
+const dbPath = path.resolve("/data", "/database.sqlite");
 
 const db = new sqlite3.Database(dbPath, (err) => {
   if (err) {
