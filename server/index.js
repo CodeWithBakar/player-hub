0a203cc1c5e419f93d2318bce99c1c7f0afaa079
abu-bakr-dev <abubakarse005@gmail.com>
2025-09-02 05:12:23 +0500

    fix : update the origin path

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const HOST = "0.0.0.0";
 app.use(
   cors({
     origin: [
-      "https://player-hub-012.netlify.app/",
+      "https://player-hub-012.netlify.app",
       "http://localhost:3000",
       "http://localhost:5173",
     ],
