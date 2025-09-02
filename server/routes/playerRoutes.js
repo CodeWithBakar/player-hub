e56769592b63f0d68f7fef1a1d98cb821ce16c93
abu-bakr-dev <abubakarse005@gmail.com>
2025-09-03 03:57:38 +0500

    feat : add player routes

--- server/routes/playerRoutes.js
+++ server/routes/playerRoutes.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const playerController = require("../controllers/playerController");
+const {
+  getAllPlayers,
+  registerPlayer,
+} = require("../controllers/playerController");
 
-// Fetches all registered players
-router.get("/", playerController.getAllPlayers);
+// This route handles GET requests to /api/players
+router.get("/players", getAllPlayers);
 
-// Registers a new player
-router.post("/register", playerController.registerPlayer);
+// This route handles POST requests to /api/register
+router.post("/register", registerPlayer);
 
 module.exports = router;
